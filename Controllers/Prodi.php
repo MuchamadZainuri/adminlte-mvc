5ad0645a7c4f89ad7d26caf65a8358a5007e2fd2
Muchamad Zainuri <muchamadzainuri6@gmail.com>
2025-03-20 12:39:43 +0700

    Refactor: update database configuration and remove unused Prodi methods and views

--- Controllers/Prodi.php
+++ Controllers/Prodi.php
@@ -10,36 +10,15 @@ class Prodi
         $this->pdo = $pdo;
     }
 
-    public function index()
-    {
-        $stmt = $this->pdo->query("SELECT * FROM prodi");
-        return $stmt->fetchAll();
-    }
+    public function index() {}
 
-    public function show($id)
-    {
-        $stmt = $this->pdo->prepare("SELECT * FROM prodi WHERE id = ?");
-        $stmt->execute([$id]);
-        return $stmt->fetch();
-    }
+    public function show($id) {}
 
-    public function create($data)
-    {
-        $stmt = $this->pdo->prepare("INSERT INTO prodi (kode,nama,kaprodi) VALUES (?,?,?)");
-        return $stmt->execute([$data['kode'], $data['nama'], $data['kaprodi']]);
-    }
+    public function create($data) {}
 
-    public function update($id, $data)
-    {
-        $stmt = $this->pdo->prepare("UPDATE prodi SET kode = ?, nama = ?, kaprodi=? WHERE id=?");
-        return $stmt->execute([$data['kode'], $data['nama'], $data['kaprodi'], $id]);
-    }
+    public function update($id, $data) {}
 
-    public function delete($id)
-    {
-        $stmt = $this->pdo->prepare("DELETE FROM prodi WHERE id = ?");
-        return $stmt->execute([$id]);
-    }
+    public function delete($id) {}
 }
 
 $prodi = new Prodi($pdo);
